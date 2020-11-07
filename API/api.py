c95a73545b7f28ad8414d732a8e3bc68172bbf71
shanks847 <shankar.ramharack@gmail.com>
2020-11-06 21:29:49 -0400

    Added API and python modules

---
+++ API/api.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+'''
+API Documentation: https://app.swaggerhub.com/apis-docs/team-dynamax/COVIZ/1.0.0#/
+'''
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"Greeting":"Hello World"} #put a stub here that calls headings from csv file on server
+
+api.add_resource(HelloWorld, "/hew")
+
+#PS DEVS: You can change the function args once you achieve functionality described in the docstring.
+
+#Denelle
+class Dropdown(Resource):
+    def get(self):
+        '''
+        Return a JSON file containg the requested headings
+        '''
+        pass
+
+#Sa'id
+class Image(Resource):
+    def post(self):
+        '''
+        Returns a JSON file containing a the SVG image as a string.
+        '''
+        pass
+
+api.add_resource(Dropdown, '/options/<datatype>')
+api.add_resource(Image, '/charts/<visualization>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
