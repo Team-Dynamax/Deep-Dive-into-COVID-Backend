298969b6371c477d5298b0e1eeae24fd79c4f9da
shanks847 <shankar.ramharack@gmail.com>
2020-11-12 03:27:11 -0400

    Finished API for OneVisual Demo

--- API/api.py
+++ API/api.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import request
 from flask_restful import Api, Resource
+import visualization as viz
+from flask import jsonify
+import preprocessing
 
 app = Flask(__name__)
 api = Api(app)
 
-'''
-API Documentation: https://app.swaggerhub.com/apis-docs/team-dynamax/COVIZ/1.0.0#/
-'''
 
-
+#Just ya typical Hi there
 class HelloWorld(Resource):
     def get(self):
         return {"Greeting":"Hello World"} #put a stub here that calls headings from csv file on server
@@ -19,22 +20,58 @@ api.add_resource(HelloWorld, "/hew")
 
 #Denelle
 class Dropdown(Resource):
-    def get(self):
+    def get(self,datatype):
         '''
         Return a JSON file containg the requested headings
-        '''
-        pass
 
-#Sa'id
+        Read the csv from firebase and collect all string columns into categorical
+        '''
+        ret = []
+        if(datatype == "numerical"):
+            ret = preprocessing.getHeadings(datatype)
+        elif(datatype == "categorical"):
+            ret = preprocessing.getHeadings(datatype)
+        else:
+            return "Invalid heading type", 400
+        
+        return {datatype:ret}, 200
+
+#Shankar
 class Image(Resource):
-    def put(self):
+    def put(self,visualization):
         '''
-        Returns a JSON file containing a the SVG image as a string.
+        Returns html string containing the visualization
         '''
-        pass
+        
+        if request.is_json:
 
-api.add_resource(Dropdown, '/options/<datatype>')
-api.add_resource(Image, '/charts/<visualization>')
+            # Parse the JSON into a Python dictionary
+            req = request.get_json()
+
+            # Print the dictionary
+            #print(req)
+
+            # Return a string along with an HTTP status code
+            # return "JSON received!", 200
+            
+            if(visualization == "lineplot"):
+                #html_text =  viz.makeLineplot(req) #how do i get the passed json file?
+                #Html_file= open("resp.html","w")
+                #Html_file.write(html_text)
+                #Html_file.close()
+                #return "Lineplot generated in index.html", 200
+                return viz.makeLineplot(req), 200
+            else:
+                return "JSON received!, but not for a lineplot", 200
+            
+
+        else:
+
+            # The request body wasn't JSON so return a 400 HTTP status code
+            return "Request was not JSON", 400
+
+api.add_resource(Dropdown, '/options/<string:datatype>')
+api.add_resource(Image, '/charts/<string:visualization>')
 
 if __name__ == '__main__':
     app.run(debug=True)
