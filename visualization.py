8e439b5192fa5f33fc5ad4a891736e541bdf4bce
shanks847 <shankar.ramharack@gmail.com>
2020-11-12 01:05:11 -0400

    init commit

--- visualization.py
+++ visualization.py
@@ -1,9 +1,15 @@
-import pygal
+#import pygal
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import firebaseInterface
 
 #Pygal DOCS: http://www.pygal.org/en/stable/documentation/
 
 
-version = '0.0.0'
+version = '0.0.1'
 
 def makeLineplot(usr_request):
     '''
@@ -14,7 +20,31 @@ def makeLineplot(usr_request):
         Returns a json file containg the svg image as a base64 encoded string
 
     '''
-    pass
+    with open(usr_request) as json_file:
+        data = json.load(json_file)
+
+        locations = data['countries']
+        feature = data['feature']
+
+        #Get the data from firebase
+        #generate the seaborn plot
+        #upload back to firebase
+        #return the image
+        #print(data)
+        firebaseInterface.fetch_file('test.csv','Cleaned Data/cleaned_data_test.csv')
+        df = pd.read_csv('test.csv')
+        #make dataframe subset of countries
+        df = df[df['location'].isin(locations)]
+
+        #make plot with subset of countries
+
+        plt.figure(figsize=(15,8))
+        sns.lineplot(data=df,x='date',y=feature,hue='location')
+        plt.gcf().autofmt_xdate()
+        plt.show()
+
+
+        
 
 
 def makeBarchart(usr_request):
@@ -51,3 +81,6 @@ def getVersion():
         Returns the version of the python module
     """
     print("Version: {}".format(version))
+
+#Testing
+makeLineplot('test.json')
